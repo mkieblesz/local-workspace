45ea2b0e5aaab30b5b2d56dba693e4d464feb079
Matt Kieblesz <mattkieblesz@hotmail.com>
2019-04-23 15:37:53 +0100

    Update log duration format display

--- scripts/config.sh
+++ scripts/config.sh
@@ -80,9 +80,9 @@ displaytime() {
     local H=$((T/60/60))
     local M=$((T/60%60))
     local S=$((T%60))
-    (( $H > 0 )) && printf '%d hours ' $H
-    (( $M > 0 )) && printf '%d minutes ' $M
-    (( $S > 0 )) && printf '%d seconds' $S
+    (( $H > 0 )) && printf '%dh ' $H
+    (( $M > 0 )) && printf '%dm ' $M
+    (( $S > 0 )) && printf '%ds' $S
 }
 
 parse_duration_log() {
